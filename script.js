2a2e84fc41bd66a09f36096e168175444fd8a8be
Thayna00 <oppakino5@hotmail.com>
2023-05-27 12:57:54 -0300

    SlideShow

---
+++ script.js
@@ -0,0 +1,38 @@
+let totalSliders = document.querySelectorAll('.slider--item').length;
+let currentSlide = 0;
+
+document.querySelector('.slider--width').style.width = 
+    `calc(100vw * ${totalSliders})`;
+
+document.querySelector('.slider--controls').style.height = 
+    `${document.querySelector('.slider').clientHeight}px`;
+/*button voltar*/
+function goPrev()
+{
+    currentSlide--;
+    if(currentSlide < 0)
+    {
+        currentSlide = totalSliders - 1;
+    }
+    updateMargin();
+}
+/*button próximo*/
+function goNext()
+{
+    currentSlide++;
+    if(currentSlide > (totalSliders - 1))
+    {
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function  updateMargin()
+{
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft =
+      `-${newMargin}px`
+}
+
+setInterval(goNext, 4000)
